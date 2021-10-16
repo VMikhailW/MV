03cf075834ea469e6a0255cc9e3d888f547cd401
V.Mikhail <V.Mikhail.V@yandex.ru>
2021-10-16 05:00:53 +0300

    16102021

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const {
-  JWT_SECRET = 'dev-secret', MONGO_ADRESS = 'mongodb://localhost:27017/moviesdb', NODE_ENV, PORT = 3001,
+  JWT_SECRET = 'dev-secret', MONGO_ADRESS = 'mongodb://localhost:27017/moviesdb', NODE_ENV, PORT = 3000,
 } = process.env;
 
 module.exports = {
